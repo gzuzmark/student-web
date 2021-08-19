1fe71d644621947540b300254c930c89a5c05890
gzuzmark <alonso.m144@gmail.com>
2021-08-19 10:30:18 -0500

    mobile view

--- src/pages/SelectDoctor/components/Carrousel/Carrousel.tsx
+++ src/pages/SelectDoctor/components/Carrousel/Carrousel.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DateCalendar from '../DateCalendar/DateCalendar';
 import useStyles from './styles';
 import LeftDisabled from 'icons/left-disabled.svg';
 import RightEnabled from 'icons/right-enabled.svg';
 import clsx from 'clsx';
+import { Theme, useMediaQuery } from '@material-ui/core';
+import { useEffect } from 'react';
 
 interface CarrouselProps {
 	dates: Date[];
@@ -11,6 +13,16 @@ interface CarrouselProps {
 
 const Carrousel = ({ dates }: CarrouselProps) => {
 	const classes = useStyles();
+	const isDesktop = useMediaQuery(({ breakpoints }: Theme) => breakpoints.up('lg'));
+	const [listDates, setListDates] = useState<Date[]>([]);
+
+	useEffect(() => {
+		if (isDesktop) {
+			setListDates(dates);
+		} else {
+			setListDates(dates.slice(0, 3));
+		}
+	}, [dates, isDesktop]);
 
 	return (
 		<div className={classes.container}>
@@ -18,7 +30,7 @@ const Carrousel = ({ dates }: CarrouselProps) => {
 				<div className={clsx(classes.pointer)}>
 					<img alt="left" src={LeftDisabled} />
 				</div>
-				{dates.map((date: Date, i: number) => (
+				{listDates.map((date: Date, i: number) => (
 					<DateCalendar date={date} key={i} />
 				))}
 				<div className={clsx(classes.pointer)}>
