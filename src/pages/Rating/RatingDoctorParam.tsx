ecf36a85a6ba6871a5ebc5c5e37af2b34a0b9276
gzuzmark <alonso.m144@gmail.com>
2021-10-29 09:38:58 -0500

    http status in post rating

--- src/pages/Rating/RatingDoctorParam.tsx
+++ src/pages/Rating/RatingDoctorParam.tsx
@@ -1,12 +1,20 @@
 import { createRatingDoctor } from 'pages/api/rating';
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router';
 
 const RatingDoctor = () => {
 	const { id } = useParams<{ id: string }>();
+	const [message, setMessage] = useState<string | null>(null);
 
 	const saveButton = async () => {
-		await createRatingDoctor(id, 3, 'Doctor es muy bueno');
+		const resp = await createRatingDoctor(id, 3, 'Doctor es muy bueno');
+		if (resp.ok) {
+			// mensaje cuando es registrado
+			setMessage(resp.message);
+		} else {
+			// mensaje cuando falla el POST
+			setMessage(resp.message);
+		}
 	};
 
 	return (
@@ -15,6 +23,7 @@ const RatingDoctor = () => {
 			<div>
 				<button onClick={saveButton}>Guardar</button>
 			</div>
+			{message != null && <span>{message}</span>}
 		</>
 	);
 };
