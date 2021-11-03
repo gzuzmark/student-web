046a399a021a03cb781061482c3a0842f7bcef59
gzuzmark <alonso.m144@gmail.com>
2021-11-03 17:13:38 -0500

    http status in post rating

--- src/pages/api/rating.ts
+++ src/pages/api/rating.ts
@@ -1,15 +1,54 @@
+import { AxiosResponse } from 'axios';
 import aliviaAxios from 'utils/customAxios';
 
-export const createRatingDoctor = async (sessionId: string, stars: number, comment: string) => {
+export interface HtppResponseRatingDoctor {
+	ok: boolean;
+	message: string;
+}
+
+export const createRatingDoctor = async (
+	sessionId: string,
+	stars: number,
+	comment: string,
+): Promise<HtppResponseRatingDoctor> => {
 	try {
 		const body = {
 			stars: stars,
 			comment: comment,
 		};
-		const response = await aliviaAxios.post(`/rating/session/${sessionId}`, body);
-		console.log(response);
+		await aliviaAxios.post(`/rating/session/${sessionId}`, body);
+		return {
+			ok: true,
+			message: 'Gracias por su calificación, ha sido registrado con éxito',
+		};
 	} catch (error) {
-		const errorAxios = error as any;
-		console.log(errorAxios.response);
+		return processHttpResponse(error as AxiosResponse);
 	}
+};
+
+const processHttpResponse = (response: AxiosResponse): HtppResponseRatingDoctor => {
+	console.log(response);
+	const status = response.status;
+	switch (status) {
+		case 404:
+			return {
+				ok: false,
+				message: 'Error, la cita médica especificada no ha sido encontrada',
+			};
+		case 422:
+			return {
+				ok: false,
+				message: 'Error, la cita médica ya ha sido calificada anteriormente',
+			};
+		case 400:
+			return {
+				ok: false,
+				message: 'Error, el contenido de la calificación tiene errores',
+			};
+	}
+
+	return {
+		ok: false,
+		message: 'Error, la calificación no ha podido ser registrado',
+	};
 };
